ba3b86b301cac340343fc1c439f38db084fe0ebd
Lucas <lucashsb12@gmail.com>
2021-05-14 20:57:11 -0300

    kdjjf

--- app/controllers/paciente.php
+++ app/controllers/paciente.php
@@ -1,24 +1,17 @@
 <?php 
-
 require_once '../../vendor/autoload.php';
 
 use \App\Model\Paciente;
-
-// if(!empty($_POST['p'])){
-// 	$p = $_POST['p'];
-// 	$paciente = new Paciente;
-// 	for($i=0; $i < count($p); $i++){
-// 		if($p[$i]['name'] == "nomePaciente"){ $paciente->nome = $p[$i]['value']; }
-// 		if($p[$i]['name'] == "telefone"){ $paciente->telefone = $p[$i]['value']; }
-// 		if($p[$i]['name'] == "celular"){ $paciente->numero = $p[$i]['value']; }
-// 	}
-// 	$paciente->create();
-// }
+if(!empty($_POST['p'])){
+	$p = $_POST['p'];
+	$paciente = new Paciente;
+	for($i=0; $i < count($p); $i++){
+		if($p[$i]['name'] == "nomePaciente"){ $paciente->nome = $p[$i]['value']; }
+		if($p[$i]['name'] == "telefone"){ $paciente->telefone = $p[$i]['value']; }
+		if($p[$i]['name'] == "celular"){ $paciente->numero = $p[$i]['value']; }
+	}
+	$paciente->create();
+}
 $paciente = new Paciente;
 
-$paciente->nome = $_POST['nome'];
-$paciente->telefone = $_POST['telefone'];
-$paciente->numero = $_POST['numero'];
-$paciente->create();
-//echo "<script> console.log($nome / $telefone / $numero) </script>";
 ?>
