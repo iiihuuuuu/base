43e709325854361c1a7b3c382fce0b3c1655733e
omegass <viniciusbarbosa@prefeitura.sp.gov.br>
2021-05-17 11:42:32 -0300

    iniciando slim para rotas

--- app/controllers/paciente.php
+++ app/controllers/paciente.php
@@ -6,12 +6,11 @@ if(!empty($_POST['p'])){
 	$p = $_POST['p'];
 	$paciente = new Paciente;
 	for($i=0; $i < count($p); $i++){
-		if($p[$i]['name'] == "nomePaciente"){ $paciente->nome = $p[$i]['value']; }
+		if($p[$i]["name"] == "nomePaciente"){ $paciente->nome = $p[$i]['value']; }
 		if($p[$i]['name'] == "telefone"){ $paciente->telefone = $p[$i]['value']; }
 		if($p[$i]['name'] == "celular"){ $paciente->numero = $p[$i]['value']; }
 	}
 	$paciente->create();
 }
-$paciente = new Paciente;
 
 ?>
--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 <?php
 
-require_once 'dashboard.php';	
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+require_once 'vendor/autoload.php';	
 // if($_GET){
 // 	$url = explode('/', $_GET['url']);
 // 	require_once $url[0].'.php';
 // }
+
+$app = new \Slim\App();
+
+$app->get('/prod', function(Request $req, Response $res, array $args){
+	$limit = $req->getQueryParams()['limit'] ?? 5;
+	return $res->getBody()->write("{$limit} produtos do banco de dados");
+});
+$app->run();
+
 
 ?>
