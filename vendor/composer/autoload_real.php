473b57671eeedc84a16e7531eb1cd5bee2e59491
luvinti <luvinti.tech@gmail.com>
2021-05-19 21:23:17 -0300

    remove slim

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -50,24 +50,6 @@ class ComposerAutoloaderInitaf49187dffbc69bfba12f5dfd4a3d476
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInitaf49187dffbc69bfba12f5dfd4a3d476::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequireaf49187dffbc69bfba12f5dfd4a3d476($fileIdentifier, $file);
-        }
-
         return $loader;
-    }
-}
-
-function composerRequireaf49187dffbc69bfba12f5dfd4a3d476($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
